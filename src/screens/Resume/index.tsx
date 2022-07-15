ab627f02a6094dd51a8870eb69be3934a8956ad5
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-07-15 14:00:36 -0300

    criando components de histórico do gráfico

---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { HistoryCard } from "../../components/HistoryCard";
+
+import * as S from './styles';
+
+export function Resume() {
+  return (
+    <S.Container>
+      <S.Header>
+        <S.Title>Resumo por categoria</S.Title>
+      </S.Header>
+
+      <HistoryCard title="Alimentação" amount="30" color="red" />
+    </S.Container>
+  )
+}
