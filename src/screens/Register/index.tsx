677243823927a07d5445c46c194eaf84926bf635
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-07-15 10:06:48 -0300

    feat: salvando novas transações utilizando o AsyncStorage

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -5,6 +5,9 @@ import { useForm } from "react-hook-form";
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
+import uuid from "react-native-uuid";
+import { useNavigation } from "@react-navigation/native";
+
 import { Button } from "../../components/Forms/Button";
 import { CategorySelectButton } from "../../components/Forms/CategorySelectButton";
 import { TransactionTypeButton } from "../../components/Forms/TransactionTypeButton";
@@ -12,30 +15,43 @@ import { CategorySelect } from "../CategorySelect";
 
 import * as S from "./styles";
 import { InputForm } from "../../components/Forms/InputForm";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface FormData {
   name: string;
   amount: string;
 }
 
+type NavigationProps = {
+  navigate: (screen: string) => void;
+};
+
 const schema = Yup.object().shape({
-  name: Yup.string().required("O nome é obrigatório"),
-  amount: Yup.number()
-    .typeError("Informe um valor")
-    .positive("O valor deve ser positivo")
+  name: Yup
+  .string()
+  .required("O nome é obrigatório"),
+  amount: Yup
+  .number()
+  .typeError("Informe um valor")
+  .positive("O valor deve ser positivo"),
 });
 
 export function Register() {
-  const [transactionType, settransactionType] = useState("");
-  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
-
+  const [transactionType, setTransactionType] = useState("");
   const [category, setCategory] = useState({
     key: "category",
     name: "Categoria",
   });
 
+  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
+  const dataKey = "@gofinance:transactions";
+
+  const navigation = useNavigation<NavigationProps>();
+
   const {
     control,
+    reset,
     handleSubmit,
     formState: { errors },
   } = useForm({
@@ -43,7 +59,7 @@ export function Register() {
   });
 
   function handleTransactionTypeSelect(type: "income" | "outcome") {
-    settransactionType(type);
+    setTransactionType(type);
   }
 
   function handleOpenSelectCategoryModal() {
@@ -54,20 +70,43 @@ export function Register() {
     setCategoryModalOpen(false);
   }
 
-  function handleRegister(form: Partial<FormData>) {
+  function clearForm() {
+    reset();
+    setTransactionType("");
+    setCategory({
+      key: "category",
+      name: "Categoria",
+    });
+    navigation.navigate("Listagem");
+  }
+
+  async function handleRegister(form: Partial<FormData>) {
     if (!transactionType) return Alert.alert("Selecione o tipo da transação");
 
     if (category.key === "category")
       return Alert.alert("Selecione a categoria");
 
-    const data = {
+    const newTransaction = {
+      id: String(uuid.v4()),
       name: form.name,
       amount: form.amount,
       transactionType,
       category: category.key,
+      date: new Date(),
     };
 
-    console.log(data);
+    try {
+      const data = await AsyncStorage.getItem(dataKey);
+      const currentData = data ? JSON.parse(data) : [];
+
+      const dataFormatted = [...currentData, newTransaction];
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+      clearForm();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possível salvar.");
+    }
   }
 
   return (
